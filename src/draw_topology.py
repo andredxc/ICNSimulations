a8fa5ee4d14cb5c6a8d7e32ec8637645b5523851
Andre Dexheimer Carneiro <acarneiro@daitan.com>
2022-03-17 15:07:14 -0300

    Adds variables to control graph font sizes

--- src/draw_topology.py
+++ src/draw_topology.py
@@ -13,6 +13,10 @@ import matplotlib.pyplot as plt
 from icnexperiment.topology_generation import Topology
 from icnexperiment.dir_config import c_strLogDir
 
+FONT_SIZE_NODE_NAME = 13
+FONT_SIZE_AXIS_LABEL = 16
+FONT_SIZE_AXIS_STEPS = 13
+SHOULD_SHOW_APS = False
 
 def main():
 
@@ -31,7 +35,6 @@ def main():
         strTopoPath = sys.argv[1]
 
     logging.info('[main] Drawing topology from path=%s' % strTopoPath)
-    # (lstNodes, lstLinks) = TopologyGenerator.readTopoFile(strTopoPath)
     pTopology = Topology.loadFromFile(strTopoPath)
 
     #################################################
@@ -50,31 +53,38 @@ def main():
         elif (pNode.getType() == 'vehicle'):
             lstVehicleCoord.append(pNode.getCoord())
 
-    lstAccessCoord = []
-    for pAP in pTopology.lstAccessPoints:
-        lstAccessCoord.append(pAP.getCoord())
-
     # Draw points
-    humanScatter   = plt.scatter([x[0] for x in lstHumanCoord], [x[1] for x in lstHumanCoord], marker='*', s=200)
-    sensorScatter  = plt.scatter([x[0] for x in lstSensorCoord], [x[1] for x in lstSensorCoord], marker='x', s=200)
-    droneScatter   = plt.scatter([x[0] for x in lstDroneCoord], [x[1] for x in lstDroneCoord], marker='^', s=200)
+    humanScatter = plt.scatter([x[0] for x in lstHumanCoord], [x[1] for x in lstHumanCoord], marker='*', s=200)
+    sensorScatter = plt.scatter([x[0] for x in lstSensorCoord], [x[1] for x in lstSensorCoord], marker='x', s=200)
+    droneScatter = plt.scatter([x[0] for x in lstDroneCoord], [x[1] for x in lstDroneCoord], marker='^', s=200)
     vehicleScatter = plt.scatter([x[0] for x in lstVehicleCoord], [x[1] for x in lstVehicleCoord], marker='s', s=200)
-    accessScatter  = plt.scatter([x[0] for x in lstAccessCoord], [x[1] for x in lstAccessCoord], c='k', marker='.', s=120)
 
     # Draw hostnames
     for pNode in pTopology.lstNodes:
-        plt.annotate(pNode.Name(), (pNode.nX, pNode.nY), xytext=(pNode.nX+800, pNode.nY+800))
+        plt.annotate(pNode.Name(), (pNode.nX, pNode.nY), xytext=(pNode.nX+800, pNode.nY+800), fontsize=FONT_SIZE_NODE_NAME)
+
+    lstScatters = [humanScatter, sensorScatter, droneScatter, vehicleScatter]
+    lstLabels = ['Soldier', 'Sensor', 'Drone', 'Vehicle']
+
+    # Draw access points, if needed
+    if SHOULD_SHOW_APS:
+        lstAccessCoord = []
+        for pAP in pTopology.lstAccessPoints:
+            lstAccessCoord.append(pAP.getCoord())
+        accessScatter = plt.scatter([x[0] for x in lstAccessCoord], [x[1] for x in lstAccessCoord], c='k', marker='.', s=120)
+        if len(lstAccessCoord) > 0:
+            lstScatters.append(accessScatter)
+            lstLabels.append('Access Point')
 
     # Draw legend
-    lstScatters = [humanScatter, sensorScatter, droneScatter, vehicleScatter]
-    lstLabels   = ['Soldier', 'Sensor', 'Drone', 'Vehicle']
-    if (len(lstAccessCoord) > 0):
-        lstScatters.append(accessScatter)
-        lstLabels.append('Access Point')
     plt.legend(lstScatters, lstLabels, loc='lower left')
 
-    plt.xlabel('Coordinate X (m)')
-    plt.ylabel('Coordinate Y (m)')
+    # Set labels and font sizes
+    plt.xlabel('Coordinate X (m)', fontsize=FONT_SIZE_AXIS_LABEL)
+    plt.ylabel('Coordinate Y (m)', fontsize=FONT_SIZE_AXIS_LABEL)
+    plt.xlim(0, 102000)
+    plt.ylim(0, 102000)
+    plt.tick_params(axis='both', which='major', labelsize=FONT_SIZE_AXIS_STEPS)
 
     #################################################
     # Draw links between nodes
